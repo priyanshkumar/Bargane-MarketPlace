e464a7a69ce6f010577d48312f085629de1cc80b
Priyansh <pradadiya667@gmail.com>
2019-05-25 07:51:42 -0400

    products model ready

---
+++ models/products.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+  var Product = sequelize.define("Product", {
+    pName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+};
