eb38c1a5c033362d78bb8a3c96b65ab55d59d1fd
Priyansh <pradadiya667@gmail.com>
2019-05-28 15:54:05 -0400

    done getting profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -3,16 +3,33 @@ var router = express.Router();
 var db = require("../models");
 var authenticated = require("../config/middelware/isAuthenticated");
 
-router.post("/api/profile", authenticated, function(req, res) {
+router.get("/profile", authenticated, function(req, res) {
+  db.Profile.findOne({
+    where: {
+      UserId: req.user.id
+    }
+  })
+    .then(function(result) {
+      data = {
+        profile: result
+      };
+      res.render("profile", data);
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.json(err);
+    });
+});
 
+router.post("/api/profile", authenticated, function(req, res) {
   db.Profile.create({
     fName: req.body.fname,
     lName: req.body.lname,
     email: req.body.email,
     phone: req.body.phone,
-    address: req.body.fname,
-    city: req.body.address,
-    PCode: req.body.city,
+    address: req.body.address,
+    city: req.body.city,
+    PCode: req.body.pcode,
     province: req.body.province,
     UserId: req.user.id
   })
